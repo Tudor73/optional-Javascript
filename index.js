5e11f402594c871e85a8e3b27fe1fe07fd72cc05
Tudor73 <tudorminciunescu@gmail.com>
2023-01-12 15:32:39 +0200

    jwt middleware

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
-
+const JWTMiddleware = require('./middlewares/JWTMiddleware');
 const app = express();
 
 app.listen(8080);
+
+app.post('/graphql', JWTMiddleware, graphqlHTTP({
+    schema: schema,
+}));
+
+    
---
+++ middlewares/JWTMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const JWT_KEY = "1158659639IFIUHSDIUSDF";
+
+const JWTMiddleware = (request, response, next) => {
+  const authorization = request.headers.authorization;
+  if (!authorization) {
+    response.status(403).send({
+      message: "Unauthorized",
+    });
+    return;
+  }
+
+  const token = authorization.replace("Bearer ", "");
+
+  try {
+    const tokenPayload = jwt.verify(token, JWT_KEY);
+    request.tokenPayload = tokenPayload;
+
+    next();
+  } catch (e) {
+    response.status(403).send({
+      message: "Unauthorized",
+    });
+    return;
+  }
+};
+
+module.exports = JWTMiddleware;
