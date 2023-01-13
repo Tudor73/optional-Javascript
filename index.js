9955bc3a55a32aa8e127e7858220c61bdef20dfd
Flavia Podariu <flaviapodariu@gmail.com>
2023-01-13 20:38:16 +0200

    updated gitignore

--- index.js
+++ index.js
@@ -5,21 +5,10 @@ const express = require("express");
 
 
 // const schema = require('./graphql');
-// app.use(express.json());
+app.use(express.json());
 
 // app.post('/graphql', JWTMiddleware, graphqlHTTP({
 //     schema: schema,
 // }));
 
-// app.listen(5000);
-const db = require('./models/index.js')
-async function main() {
-    try{
-        await db.sequelize.sync({ force: true });
-    }
-    catch(e) {
-        console.log(e)
-    }
-}
-
-main().then(console.log("All models were synchronized successfully."))
+app.listen(5000);
